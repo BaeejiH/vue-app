fbedc9004732556572871206d39ff002a15fa296
BaeejiH <bjihun0511@gmail.com>
2024-11-01 14:23:06 +0900

    vue 화면쪽 틀잡기

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,7 +1,32 @@
 <template>
-    <div>
-        <h1>ListView!!!!!!1</h1>
+   <div class="container mt-3">
+    <h1 class="display-1 text-center">사용자 목록</h1>
+    <div class="btn-group">
+      <a href="/user/save" class="btn btn-primary">사용자 추가</a>
     </div>
+    <table class="table table-hover mt-3">
+      <thead class="table-dark">
+        <tr>
+          <th>이름</th>
+          <th>이메일</th>
+          <th>가입날짜</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="cursor-pointer" onclick = "location.href ='/user/findById';">
+          <td>스티븐</td>
+          <td>xxxxx@gmail.com</td>
+          <td>2023-02-07</td>
+        </tr>
+
+        <tr class="cursor-pointer"  onclick = "location.href ='/user/findById';">
+          <td>제라드</td>
+          <td>yyyyy@gmail.com</td>
+          <td>2023-01-07</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
